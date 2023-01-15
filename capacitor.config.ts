4f3dc37f733d08b280c081febe1193b19e4b12ad
Etopick.com <zia.kcst@gmail.com>
2023-01-15 16:16:19 +0600

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'LovedOnez',
-  webDir: 'www',
+  webDir: 'http://lovedOnez.etopick.com',
   bundledWebRuntime: false
 };
 
